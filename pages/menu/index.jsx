30c4a30da9bb2fe220443c9b173a4032a65a5f9e
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-08-17 00:55:08 +0330

    feat: Added BASE_URL and REVALIDATE environment variables to .env file and create the pages

---
+++ pages/menu/index.jsx
@@ -0,0 +1,16 @@
+import MenuPage from "../../components/templates/MenuPage";
+
+function index({ data }) {
+  return <MenuPage data={data} />;
+}
+
+export default index;
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.BASE_URL}/data`);
+  const data = await res.json();
+  return {
+    props: { data },
+    revalidate: 3600,
+  };
+}
